6765e59b32d5b121000ab37e69ac9467458faa30
Wellington <spaaws@gmail.com>
2022-08-02 21:21:55 -0300

    implementacao do endpoint events

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Event struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type allEvents []Event
+
+var events = allEvents{
+	{ID: 1, Title: "Unileão DAY", Description: "Evento para alunos das escolas"},
+	{ID: 2, Title: "App Projeto Integrador", Description: "Apresentação do projeto integrador"},
+}
+
 func main() {
 	//access log
 	log.Println("Starting API...")
@@ -16,6 +30,7 @@ func main() {
 	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 	router.HandleFunc("/", Home)
+	router.HandleFunc("/events", GetAllEvents).Methods("GET")
 	// http.ListenAndServe(":8080", router) // test localhost:8080/
 	http.ListenAndServe(":"+port, router) // test heroku
 }
@@ -23,4 +38,11 @@ func main() {
 func Home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Home endpoint accessed")
 	fmt.Fprintf(w, "Aplication is running")
+}
+
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {
+	log.Println("GetAllEvents endpoint accessed")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(events)
 }
